b549ea4cac0db566c031c359463061193bf21b86
Dante Huaman <danraf77@hotmail.com>
2021-09-06 10:16:00 -0500

    importaciones tech + Dante Huaman

---
+++ src/components/Nav.js
@@ -0,0 +1,11 @@
+const Nav = () => {
+    return (
+            <nav>
+                <a href="#">Computo</a>
+                <a href="#">Tablets</a>
+                <a href="#">Accesorios</a>
+                <a href="#">Relojes</a>
+            </nav>
+    )
+}
+export default Nav
